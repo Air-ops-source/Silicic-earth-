d4aa32a4298094841633e2858b7bc69a2a9a8ba7
Air-ops-source <abrahamtrabajo17@gmail.com>
2025-12-03 16:07:23 +0100

    Actualizar app.py

--- app.py
+++ app.py
@@ -5,9 +5,5 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "🌍 Silicic Earth API", "status": "online"})
 
-@app.route('/estadisticas')
-def stats():
-    return jsonify({"total_evaluaciones": 0, "puntuacion_promedio": 0})
-
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=10000)
